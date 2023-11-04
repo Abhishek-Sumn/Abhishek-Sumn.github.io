d61546470039e236ac6fdbc3e83669def595b8dc
Abhishek suman <abhisheksumancode@gmail.com>
2023-11-04 20:49:28 +0530

    major upgrade 2 next 14

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,10 @@
-//1
 
-'use client'
-import Contact from "./components/Contact";
-import Hero from "./components/Hero";
-import Who from "./components/Who";
-import Works from "./components/Works";
-import styled from "styled-components";
-
-import { useRef } from "react";
-import Navbar from './components/Navbar'
-import "./page.module.css"
-
-const Container = styled.div`
-height:100vh;
-scroll-snap-type: y mandatory;
-scroll-behavior:smooth;
-overflow-y:auto;
-scrollbar-width: none;
-/* &::-webkit-scrollbar{
-  display:none;
-} */
-color:white;
-background:url("./img/bg1.jpg");
-
-
-
-`
-
+import styles from './page.module.css'
+import App from './App.jsx'
 export default function Home() {
-  const worksRef = useRef(null);
-  const studioRef = useRef(null);
-  const contactRef = useRef(null);
-
-
   return (
-
-    <Container>
-
-      <Navbar boy={worksRef} />
-      <Hero />
-      <Who />
-      <Works ref={worksRef} />
-      <Contact />
-
-    </Container>
+    
+      <App />
+    
   )
 }
